4e7995e28fa9a38e5fad96a78a0eecf9777f99e1
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-10 18:52:01 +0200

    again trying to fix unicode problem

--- src/components/funcComponents/Question.js
+++ src/components/funcComponents/Question.js
@@ -1,8 +1,21 @@
+import { useEffect, useState } from 'react';
+import './Question.css';
+
 function Question(props){
-    console.log(props);
+    const [questionText, setQuestionText] = useState(""); 
+    
+
+
+    useEffect(() => {
+        // .from(b64, "base64").toString();
+        let questionAsSTRING = decodeURI(props.question).toString();
+        setQuestionText(questionAsSTRING);
+        console.log(questionAsSTRING);
+    }, [questionText])
+
     return (
-        <div>
-            <h1>{props.question.question}</h1>
+        <div className="ratata">
+            <h1>{questionText}</h1>
         </div>
     );
 }

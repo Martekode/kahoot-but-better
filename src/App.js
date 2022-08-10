3fbafa32b39ecbba3d484be022b0d06e6807cbd8
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-10 11:33:07 +0200

    repositioning of header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
-        <Header/>
       </header>
+
+      <Footer/>
     </div>
   );
 }

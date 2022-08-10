ce5ea2e842cf579f178b5a8d40c8a41293483d03
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-10 15:47:07 +0200

    import and nesting of the game component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Game from './components/funcComponents/Game';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <header className="App-header">
+        <Game/>
       </header>
 
       <Footer/>

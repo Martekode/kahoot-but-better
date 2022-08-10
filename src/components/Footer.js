1319b29baa6c93b66f6af2b46427a15d4a7cd619
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-10 11:33:58 +0200

    footer component cerated

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer(){
+    const footerTitle = "made by Martekode The Great!"
+
+    return (
+        <div className='footer'>
+            {footerTitle}
+        </div>
+    );
+}
+export default Footer

ec54fd21278461a17b0b4ced939bf1fe72fdc0a5
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-10 18:51:23 +0200

    trying to fix unicode problem

--- src/components/funcComponents/Game.js
+++ src/components/funcComponents/Game.js
@@ -3,14 +3,14 @@ import { useState } from 'react';
 import Question from './Question';
 
 function Game(){
-    const url = "https://opentdb.com/api.php?amount=10&category=20";
+    const url = "https://opentdb.com/api.php?amount=10&category=20&encode=url3986";
 
     const [result , setResult] = useState([]);
 //something seems awfully hardcoded here, if only the user could change these settings!
     const getQuestions = async () => {
         const res = await  fetch(url);
         const data = await res.json();
-        console.log(data);
+        console.log(data.results);
         setResult(data.results);
     }
     return (
@@ -19,7 +19,7 @@ function Game(){
             <div className={'Questions'}>
                 {
                 result.map((question, i) => {
-                    return <Question question={question} key={i}/>
+                    return <Question question={question.question} key={i}/>
                 })}
             </div>
         </div>

0505477a89f806b7bc82c1437aeb2fc61d1db957
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-12 12:11:57 +0200

    onclick made for styling

--- src/components/funcComponents/SingleAnswer.jsx
+++ src/components/funcComponents/SingleAnswer.jsx
@@ -5,6 +5,7 @@ class SingleAnswer extends React.Component{
         super(props);
         this.state = {
             hover : false,
+            clicker : false,
             styles: {
                 listStyleHover: {
                     display: "inline-block",
@@ -58,26 +59,37 @@ class SingleAnswer extends React.Component{
         console.log("leave happy")
         this.setState({hover : false,});
     }
-
-    placeStylingLi(hover){
-        if(!hover){
-            return this.state.styles.listStyle;
+    click = () => {
+        if (!this.state.clicker){
+            console.log("clicked")
+            console.log(this.state.clicker)
+            this.setState({clicker : true,});
+            console.log(this.state.clicker)
         }else{
+            this.setState({clicker : false,});
+            console.log(this.state.clicker)
+        }
+
+    }
+    placeStylingLi(hover,clicker){
+        if(!hover && !clicker){
+            return this.state.styles.listStyle;
+        }else if(hover || clicker){
             return this.state.styles.listStyleHover;
         }
     }
-    placeStylingP(hover){
-        if(!hover){
+    placeStylingP(hover,clicker){
+        if(!hover && !clicker){
             return this.state.styles.noSelectStyle;
-        }else{
+        }else if(hover || clicker){
             return this.state.styles.selectedStyle;
         }
     }
     render(){
         return (
-            <li onMouseEnter={this.enter} onMouseLeave={this.leave} style={this.placeStylingLi(this.state.hover)}>
+            <li onClick={this.click} onMouseEnter={this.enter} onMouseLeave={this.leave} style={this.placeStylingLi(this.state.hover,this.state.clicker)}>
                 <p>{decode(this.props.answer)}</p>
-                <p style={this.placeStylingP(this.state.hover)}>{this.state.hover ? "select" : "no select"}</p>
+                <p style={this.placeStylingP(this.state.hover,this.state.clicker)}>{this.state.hover || this.state.clicker ? "select" : "no select"}</p>
             </li>
         );
     }

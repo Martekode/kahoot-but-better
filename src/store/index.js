1632602cab1c7ae06e3d31aa15f54411a7d7e2b2
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-12 15:50:28 +0200

    switch case

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,21 @@ const initialState = {
     answers : []
 }
 const reducer = (state = initialState ,action)=>{
-
+    switch (action.type) {
+        case 'ANSWER_REQUEST':
+            return Object.assign({}, state, {
+                isLoading: action.payload.isLoading
+            })
+        case 'ANSWER_REQUEST_SUCCESS':
+            return Object.assign({}, state, {
+                answers: state.answers.concat(action.answers),
+                isLoading: action.isLoading
+            })
+        default:
+            return state;
+        }
 }
 const store = configureStore({
-  reducer
+    reducer
 })
 export default store;
